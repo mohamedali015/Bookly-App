6e5ff1cc4fc47d47bc39c1ad246aa1f0adf0da5e
Mohamed Ali <mohamed.ali01537@gmail.com>
2025-09-14 02:14:24 +0300

    app init files

---
+++ lib/core/utils/app_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'app_constrains.dart';
+
+class AppTheme {
+  static final ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: AppConstants.fontFamily,
+  );
+}
