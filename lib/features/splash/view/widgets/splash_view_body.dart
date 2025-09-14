3b42ca0cc3d2678911ca2309af6a42727b5977fe
Mohamed Ali <mohamed.ali01537@gmail.com>
2025-09-14 17:33:48 +0300

    add text to splash view

--- lib/features/splash/view/widgets/splash_view_body.dart
+++ lib/features/splash/view/widgets/splash_view_body.dart
@@ -1,5 +1,7 @@
+import 'package:bookly/core/helper/my_responsive.dart';
 import 'package:bookly/core/utils/app_assets.dart';
 import 'package:flutter/material.dart';
+
 
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
@@ -12,6 +14,12 @@ class SplashViewBody extends StatelessWidget {
       children: [
         Image.asset(
           AppAssets.logo,
+        ),
+        SizedBox(height: MyResponsive.height(context, value: 10)),
+        Text(
+          'Read Free Books',
+          textAlign: TextAlign.center,
+          style: Theme.of(context).textTheme.titleMedium,
         ),
       ],
     );
