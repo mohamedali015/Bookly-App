12a756618cfd2c42a6e7e6149ecf68b6af0f8358
Mohamed Ali <mohamed.ali01537@gmail.com>
2025-09-15 20:36:32 +0300

    add google fonts family (inter) to app theme

--- lib/core/utils/app_theme.dart
+++ lib/core/utils/app_theme.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'app_colors.dart';
 import 'app_constrains.dart';
 
 class AppTheme {
   static final ThemeData lightTheme = ThemeData(
     useMaterial3: true,
-    fontFamily: AppConstants.fontFamily,
+    textTheme: GoogleFonts.interTextTheme(ThemeData.dark().textTheme),
     scaffoldBackgroundColor: AppColors.primary,
     brightness: Brightness.dark,
   );
