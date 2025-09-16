622dc8d55f8b5a9e40d07795a08e32626370ebca
Mohamed Ali <mohamed.ali01537@gmail.com>
2025-09-16 20:15:15 +0300

    create custom book details abb bar

--- lib/features/book_details/view/widgets/book_details_view_body.dart
+++ lib/features/book_details/view/widgets/book_details_view_body.dart
@@ -1,10 +1,20 @@
+import 'package:bookly/core/helper/my_responsive.dart';
 import 'package:flutter/material.dart';
+
+import 'custom_book_details_app_bar.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: MyResponsive.paddingOnly(left: 30, right: 30, top: 40),
+      child: Column(
+        children: [
+          CustomBookDetailsAppBar(),
+        ],
+      ),
+    );
   }
 }
