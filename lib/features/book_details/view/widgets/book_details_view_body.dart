50a48a443933cf9fcd94d69a90de92ff8a645023
Mohamed Ali <mohamed.ali01537@gmail.com>
2025-09-17 04:36:56 +0300

    fix book details screen with custom scroll view

--- lib/features/book_details/view/widgets/book_details_view_body.dart
+++ lib/features/book_details/view/widgets/book_details_view_body.dart
@@ -17,62 +17,78 @@ class BookDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: MyResponsive.paddingOnly(left: 30, right: 30, top: 40),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const CustomBookDetailsAppBar(),
-          SizedBox(
-            height: MyResponsive.height(value: 36),
+      padding: MyResponsive.paddingOnly(
+        left: 30,
+        right: 30,
+      ),
+      child: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            pinned: true,
+            automaticallyImplyLeading: false,
+            backgroundColor: AppColors.primary,
+            surfaceTintColor: AppColors.transparent,
+            titleSpacing: 0,
+            title: const CustomBookDetailsAppBar(),
           ),
-          Padding(
-            padding: MyResponsive.paddingSymmetric(horizontal: 76),
-            child: CustomBookImage(
-              image: AssetImage(AppAssets.bookImageTest),
-              aspectRatio: 162 / 210,
+          SliverToBoxAdapter(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: MyResponsive.height(value: 36),
+                ),
+                Padding(
+                  padding: MyResponsive.paddingSymmetric(horizontal: 76),
+                  child: CustomBookImage(
+                    image: AssetImage(AppAssets.bookImageTest),
+                    aspectRatio: 162 / 210,
+                  ),
+                ),
+                SizedBox(
+                  height: MyResponsive.height(value: 42),
+                ),
+                Text(
+                  'The Jungle Book',
+                  style: AppFontStyles.textF30(),
+                ),
+                SizedBox(
+                  height: MyResponsive.height(value: 13),
+                ),
+                Text(
+                  'Rudyard Kipling',
+                  style: AppFontStyles.textF18().copyWith(
+                    color: AppColors.grey,
+                    fontWeight: FontWeight.w500,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+                SizedBox(
+                  height: MyResponsive.height(value: 20),
+                ),
+                BookRating(),
+                SizedBox(
+                  height: MyResponsive.height(value: 37),
+                ),
+                const BookAction(),
+                SizedBox(
+                  height: MyResponsive.height(value: 50),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    AppStrings.youCanAlsoLike,
+                    style: AppFontStyles.textF16()
+                        .copyWith(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                SizedBox(
+                  height: MyResponsive.height(value: 20),
+                ),
+                const SimilarBooksListView(),
+              ],
             ),
-          ),
-          SizedBox(
-            height: MyResponsive.height(value: 42),
-          ),
-          Text(
-            'The Jungle Book',
-            style: AppFontStyles.textF30(),
-          ),
-          SizedBox(
-            height: MyResponsive.height(value: 13),
-          ),
-          Text(
-            'Rudyard Kipling',
-            style: AppFontStyles.textF18().copyWith(
-              color: AppColors.grey,
-              fontWeight: FontWeight.w500,
-              fontStyle: FontStyle.italic,
-            ),
-          ),
-          SizedBox(
-            height: MyResponsive.height(value: 20),
-          ),
-          BookRating(),
-          SizedBox(
-            height: MyResponsive.height(value: 37),
-          ),
-          const BookAction(),
-          SizedBox(
-            height: MyResponsive.height(value: 50),
-          ),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              AppStrings.youCanAlsoLike,
-              style:
-                  AppFontStyles.textF16().copyWith(fontWeight: FontWeight.bold),
-            ),
-          ),
-          SizedBox(
-            height: MyResponsive.height(value: 20),
-          ),
-          const SimilarBooksListView(),
+          )
         ],
       ),
     );
