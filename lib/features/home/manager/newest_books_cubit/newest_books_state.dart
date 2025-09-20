9e7e13f5760c1553fd612b6750666e4446073f0d
Mohamed Ali <mohamed.ali01537@gmail.com>
2025-09-20 18:10:14 +0300

    create Newest books cubit & state

---
+++ lib/features/home/manager/newest_books_cubit/newest_books_state.dart
@@ -0,0 +1,19 @@
+import 'package:bookly/features/home/data/models/book_model/book_model.dart';
+
+abstract class NewestBooksState {}
+
+class NewestBooksInitial extends NewestBooksState {}
+
+class NewestBooksLoading extends NewestBooksState {}
+
+class NewestBooksSuccess extends NewestBooksState {
+  final List<BookModel> books;
+
+  NewestBooksSuccess(this.books);
+}
+
+class NewestBooksFailure extends NewestBooksState {
+  final String errMessage;
+
+  NewestBooksFailure(this.errMessage);
+}
